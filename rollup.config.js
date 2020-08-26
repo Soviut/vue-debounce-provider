2b83354bc0c751b494aa8fc9df15c3853d226f13
Ian Zamojc <izamojc@zamtools.com>
2020-08-26 02:52:20 -0400

    rollup

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import buble from 'rollup-plugin-buble'
+import cleanup from 'rollup-plugin-cleanup'
+import filesize from 'rollup-plugin-filesize'
+import pkg from './package.json'
+
+const banner =
+  `/**
+ * Vue Debounce Provider ${pkg.version}
+ * (c) 2020-${new Date().getFullYear()} ${pkg.author}
+ * @license ${pkg.license}
+ */`
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      format: 'esm',
+      file: pkg.module,
+      exports: 'named',
+      banner
+    },
+    {
+      name: 'VueDebounceProvider',
+      format: 'umd',
+      exports: 'named',
+      file: pkg.main,
+      globals: {
+        vue: 'Vue'
+      },
+      banner
+    }
+  ],
+  plugins: [
+    buble({
+      objectAssign: true
+    }),
+    cleanup(),
+    filesize()
+  ],
+  external: ['vue']
+}
