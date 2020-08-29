488330de45b564e9b9709a9a8b6f92dd31e476ac
Ian Zamojc <izamojc@zamtools.com>
2020-08-29 18:50:42 -0400

    start event

--- tests/unit/component.spec.js
+++ tests/unit/component.spec.js
@@ -16,6 +16,57 @@ const mountComponent = (config => shallowMount(Debounce, {
   }
 }))
 
+describe('@start', () => {
+  describe('when not debouncing', () => {
+    it('should be evoked immediately after debounce is called', () => {
+      const onStart = jest.fn()
+
+      const wrapper = mountComponent({
+        listeners: { start: onStart }
+      })
+
+      wrapper.find('button.start').trigger('click')
+      expect(onStart).toHaveBeenCalled()
+    })
+  })
+
+  describe('when already debouncing', () => {
+    it('should not evoke after debounce is called', () => {
+      const onStart = jest.fn()
+
+      const wrapper = mountComponent({
+        propsData: { wait: 300 },
+        listeners: { start: onStart }
+      })
+
+      wrapper.find('button.start').trigger('click')
+      expect(onStart).toHaveBeenCalledTimes(1)
+
+      jest.advanceTimersByTime(150)
+      wrapper.find('button.start').trigger('click')
+      expect(onStart).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('when debouncing is complete', () => {
+    it('should evoke immediately after debounce is called', () => {
+      const onStart = jest.fn()
+
+      const wrapper = mountComponent({
+        propsData: { wait: 300 },
+        listeners: { start: onStart }
+      })
+
+      wrapper.find('button.start').trigger('click')
+      expect(onStart).toHaveBeenCalledTimes(1)
+
+      jest.advanceTimersByTime(300)
+      wrapper.find('button.start').trigger('click')
+      expect(onStart).toHaveBeenCalledTimes(2)
+    })
+  })
+})
+
 describe('@timeout', () => {
   describe('when :wait is default (0)', () => {
     it('should be evoked immediately after debounce is called', () => {
@@ -53,7 +104,24 @@ describe('@timeout', () => {
         listeners: { timeout: onTimeout }
       })
 
-      expect.assertions(3)
+      wrapper.find('button.start').trigger('click')
+      expect(onTimeout).not.toHaveBeenCalled()
+
+      // assert it has not been called midway
+      jest.advanceTimersByTime(150)
+      expect(onTimeout).not.toHaveBeenCalled()
+
+      jest.advanceTimersByTime(150)
+      expect(onTimeout).toHaveBeenCalled()
+    })
+
+    it('should be evoked after 500ms after debounce is called twice', () => {
+      const onTimeout = jest.fn()
+
+      const wrapper = mountComponent({
+        propsData: { wait: 300 },
+        listeners: { timeout: onTimeout }
+      })
 
       wrapper.find('button.start').trigger('click')
       expect(onTimeout).not.toHaveBeenCalled()
