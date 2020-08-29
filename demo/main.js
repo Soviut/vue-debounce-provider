4e788e245a8a389791339bf57fe26454657a0a62
Ian Zamojc <izamojc@zamtools.com>
2020-08-29 00:32:59 -0400

    fixed naming

--- demo/main.js
+++ demo/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import DebounceProvider from '../src'
+import VueDebounceProvider from '../src'
 
 Vue.config.productionTip = false
 
-Vue.use(DebounceProvider)
+Vue.use(VueDebounceProvider)
 
 new Vue({
   render: h => h(App),
